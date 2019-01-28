c3cc48bb988537efccd7a945a3edaf4e5e0e928e
diggsn <42653979+DiggsNG@users.noreply.github.com>
2019-01-27 21:08:50 -0800

    JAWSDB Update

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,11 @@
 var mysql = require("mysql");
-//var inquirer = require("inquirer");
+var connection;
 
 // create the connection information for the sql database
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+ connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
@@ -15,7 +18,7 @@ var connection = mysql.createConnection({
   password: "root",
   database: "burgers_db"
 });
-
+};
 // connect to the mysql server and sql database
 connection.connect(function(err) {
   if (err) throw err;
